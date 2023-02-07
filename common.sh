20252c708b1b779965c9f28efd96b9a5e2cf216b
Mithun Tunga <mithunreddythunga@gmail.com>
2023-02-07 14:16:22 -0600

    moving scriptLocation and log to common shell script and giving a location to log

--- common.sh
+++ common.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
+
+scriptLocation=$(pwd)
+Log=$(/tmp/roboshop.log)
+
 print_head() {
   echo -e "\e[31m$1\e[0m"
 }
--- frontend.sh
+++ frontend.sh
@@ -1,30 +1,28 @@
 #!/usr/bin/env bash
 source common.sh
 
-scriptLocation=$(pwd)
-log=$(log)
 
 print_head "Installing Nginx"
-yum install nginx -y &> "${log}"
+yum install nginx -y &> "${Log}"
 
 print_head "Enabling Nginx"
-systemctl enable nginx &> "${log}"
+systemctl enable nginx &> "${Log}"
 
 print_head "Start Nginx"
-systemctl start nginx &> "${log}"
+systemctl start nginx &> "${Log}"
 
 print_head "Cleaning Nginx old content"
-rm -rf /usr/share/nginx/html/* &> "${log}"
+rm -rf /usr/share/nginx/html/* &> "${Log}"
 
 print_head "Downloading frontend project"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &> "${log}"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &> "${Log}"
 
 print_head "Extracting the frontend code to Nginx/html where the code gets served"
-cd /usr/share/nginx/html &> "${log}"
-unzip /tmp/frontend.zip &> "${log}"
+cd /usr/share/nginx/html &> "${Log}"
+unzip /tmp/frontend.zip &> "${Log}"
 
 print_head "Creating roboshop configuration"
-cp "${scriptLocation}"/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &> "${log}"
+cp "${scriptLocation}"/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &> "${Log}"
 
 print_head "Restarting Nginx"
-systemctl restart nginx &> "${log}"
+systemctl restart nginx &> "${Log}"
