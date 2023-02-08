961d63ebb8e3b94895461e416fd063e4be6f2a21
Mithun Tunga <mithunreddythunga@gmail.com>
2023-02-07 22:38:58 -0600

    adding status check function

--- common.sh
+++ common.sh
@@ -6,3 +6,12 @@ log=/tmp/roboshop.log
 print_head() {
   echo -e "\e[31m$1\e[0m"
 }
+
+status_check() {
+  if [ $? -eq 0 ];
+    then
+      echo -e "\e[33mSUCCESS\e[0m"
+    else
+      echo -e "\e[31mFAILURE\e[0m"
+  fi
+}
--- frontend.sh
+++ frontend.sh
@@ -3,26 +3,34 @@ source common.sh
 
 
 print_head "Installing Nginx"
-yum install nginx -y &>"${log}"
+yum install nginx -y &>${log}
+status_check
 
 print_head "Enabling Nginx"
-systemctl enable nginx &>$"{log}"
+systemctl enable nginx &>${log}
+status_check
 
 print_head "Start Nginx"
-systemctl start nginx &>$"{log}"
+systemctl start nginx &>${log}
+status_check
 
 print_head "Cleaning Nginx old content"
-rm -rf /usr/share/nginx/html/* &>$"{log}"
+rm -rf /usr/share/nginx/html/* &>${log}
+status_check
 
 print_head "Downloading frontend project"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>$"{log}"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>${log}
+status_check
 
 print_head "Extracting the frontend code to Nginx/html where the code gets served"
-cd /usr/share/nginx/html &>$"{log}"
-unzip /tmp/frontend.zip &>$"{log}"
+cd /usr/share/nginx/html &>${log}
+unzip /tmp/frontend.zip &>${log}
+status_check
 
 print_head "Creating roboshop configuration"
-cp "${scriptLocation}"/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>$"{log}"
+cp "${scriptLocation}"/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>${log}
+status_check
 
 print_head "Restarting Nginx"
-systemctl restart nginx &>$"{log}"
+systemctl restart nginx &>${log}
+status_check
