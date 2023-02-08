b13c18282fc9cfbf57e557ceef067fe9edcb84b5
Mithun Tunga <mithunreddythunga@gmail.com>
2023-02-08 14:51:31 -0600

    adding $ to catalogue shell script

--- catalogue.sh
+++ catalogue.sh
@@ -2,15 +2,15 @@
 source common.sh
 
 print_head "Downloading Node rpm"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>{log}
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
 status_check
 
 print_head "Installing Nodejs"
-yum install nodejs -y &>>{log}
+yum install nodejs -y &>>${log}
 status_check
 
 print_head "Adding user"
-useradd roboshop &>>{log}
+useradd roboshop &>>${log}
 id roboshop
 if [ $? -eq 0 ];
   then
@@ -22,44 +22,45 @@ fi
 
 print_head "creating app directory"
 mkdir /app
-status_check
-if [ $? -eq 0  ];
-  then rm -rf
+if [ $? -eq 0 ];
+  then
+    rm -rf
 fi
+status_check
 
 print_head "Domloading content"
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>{log}
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log}
 status_check
 
 print_head "Switching directory and unzipping content"
-cd /app  &>>{log}
-unzip /tmp/catalogue.zip &>>{log}
+cd /app  &>>${log}
+unzip /tmp/catalogue.zip &>>${log}
 status_check
 
 print_head "Installing Npm"
-npm install &>>{log}
+npm install &>>${log}
 status_check
 
 print_head "Creating catalogue service"
-cp "${scriptLocation}"/files/catalogue.service /etc/systemd/system/catalogue.service &>>{log}
+cp "${scriptLocation}"/files/catalogue.service /etc/systemd/system/catalogue.service &>>${log}
 status_check
 
 print_head "Deamon reload"
-systemctl daemon-reload &>>{log}
+systemctl daemon-reload &>>${log}
 status_check
 
 print_head "Enable Catalogue"
-systemctl enable catalogue &>>{log}
+systemctl enable catalogue &>>${log}
 status_check
 
 print_head "Start Catalogue"
-systemctl start catalogue &>>{log}
+systemctl start catalogue &>>${log}
 status_check
 
 mongo
 
 print_head "Installing mongo shell"
-yum install mongodb-org-shell -y &>>{log}
+yum install mongodb-org-shell -y &>>${log}
 status_check
 
-mongo --host mongodb-dev.mithundevops.online </app/schema/catalogue.js &>>{log}
+mongo --host mongodb-dev.mithundevops.online </app/schema/catalogue.js &>>${log}
