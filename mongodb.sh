ef2748a8c7361787a3ab098e9069cffe4a4eb5c9
Mithun Tunga <mithunreddythunga@gmail.com>
2023-02-08 14:13:20 -0600

    updating log append for mongd

--- mongodb.sh
+++ mongodb.sh
@@ -5,17 +5,17 @@ source common.sh
 mongo
 
 print_head "Installing mongod"
-yum install mongodb-org -y &>{log}
+yum install mongodb-org -y &>>{log}
 status_check
 
 print_head "Changing default configuration"
-sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>{log}
+sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>{log}
 status_check
 
 print_head "Enabling mongod"
-systemctl enable mongod &>{log}
+systemctl enable mongod &>>{log}
 status_check
 
 print_head "Restarting Mongod"
-systemctl restart mongod &>{log}
+systemctl restart mongod &>>{log}
 status_check
